ff7ffa0234c5e0b5c1b68a45608ed9446056f6bb
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2024-12-17 13:06:02 -0300

    Adicionado o roteamento na apresentação

--- api/src/Apresentacao.php
+++ api/src/Apresentacao.php
@@ -6,13 +6,23 @@ class Apresentacao {
 
     public static function apresentacao() {
         $response = [
-            'autores' => ['Autor 1', 'Autor 2'],
+            'autores' => ['Alessandra Freitas Pacheco', 'Renata Oliveira Schäfer'],
             'rotas' => [
-                'GET /api/denuncias',  
-                'POST /api/denuncias', 
-                'GET /api/denuncia/:id',  
-                'DELETE /api/denuncia/:id', 
-                'GET /api/courses/:id/usuarios', 
+                'GET /api/denuncias',  //Listagem
+                'POST /api/denuncias', //Criar
+                'GET /api/denuncia/:id',  //Listar ID
+                'POST /api/denuncias/:id', //Editar ID
+                'DELETE /api/denuncia/:id',
+                'GET /api/denuncias?status={status}', //Filtrar status
+                'GET /api/denuncias?titulo={titulo}', //Filtrar titulo
+                'GET /api/usuarios', //Listagem
+                'POST /api/usuarios', //Criar
+                'GET /api/usuarios/:id',// Listar ID
+                'PUT /api/usuarios/:id', //Editar ID
+                'DELETE /api/usuarios/:id', //Delete ID
+                'GET /api/usuarios?nome_usuario={nome_usuario}', //Filtrar nome
+                'GET /api/usuarios?cpf_usuario={cpf_usuario}', //Filtrar cpf
+                'GET /api/usuarios?telefone={telefone}', //Filtrar telefone
                 'GET /api/db',
                 'GET /api' 
             ]
